14ee30aa464b0abbf929a7a1c445c9bed0588914
WelBert-dev <wellison.bertelli@hotmail.com>
2022-11-02 18:37:07 -0300

    Finaliza o basico e add passo a passo

--- demo/banco/dependenciasIniciais.sql
+++ demo/banco/dependenciasIniciais.sql
@@ -1,2 +1,2 @@
-use 
+use DB_LG3_RestauranteIFSP;
 insert into usuario (id, email, first_name, last_name, login, password, perfil_permissao) values (1, 'wellison.bertelli@hotmail.com', 'wellison', 'bertelli', 'welbert', 'wb123#', '1');
